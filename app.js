2f261f317ddb59c9f89613fa0771504e153e91a3
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-02-27 13:36:56 +1100

    java practice

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+ *Converting Temperatures
+ *
+ *Celsius-to-Fahrenheight formula:
+ *
+ *     F = C x 1.8 + 32
+ */
+
+ let Celsius = 10
+ let Fahrenheight = Celsius * 1.8 + 32
+
+ console.log(Fahrenheight)
+ aadada
