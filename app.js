4042171bae9b78b75cd2d3443b05d3bdecac3701
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-03-03 21:41:04 +1100

    practice

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ let cash = 45
 let price = 40
 let difference = cash - price 
 
+if (cash > price) {
+    console.log(`you paid extra - here's ${difference} dollars change`)
+}
+
+else if (cash === price) {
+    console.log('you paid the exact amount, have a nice day!') 
+}
+
+else {
+    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+}
