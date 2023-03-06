e6fff13c9a2978afb8e32e82bcf1874fb2886353
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-03-06 13:41:46 +1100

    prc

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-let count = 1;
 
-while (count <= 100) {
-console.log(count)
-count = count + 1;
-
-
-
+for (let i = 1; i <= 20; ++i) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`) 
+    }
+    else if (i % 3 === 0) {
+        console.log(`${i} -> Frontend`)
+    }
+else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
 }
-
-for (let i = 0; i < 3; i++) {
-console.log(i)
 }
