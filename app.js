c676bd50e22195a2fbcb1af07070b69dc69441d2
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-03-06 14:53:59 +1100

    prc

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
+consr str = "Frontend Simplified"
 
-for (let i = 1; i <= 20; ++i) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log(`${i} -> Frontend Simplified`) 
-    }
-    else if (i % 3 === 0) {
-        console.log(`${i} -> Frontend`)
-    }
-    else if (i % 5 === 0) {
-    console.log(`${i} -> implified`)
-    }
-    else {
-    console.log(`${i} -> Frontend`)}
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
 }
