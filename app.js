1162992019314a9f53cac0307daadef6651bbf03
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-03-07 14:41:04 +1100

    Function prac

--- app.js
+++ app.js
@@ -2,3 +2,7 @@
 function welcomepersonToFES(firstname, Lastname) {
     console.log(ˋWelcome to FES, ${firstname} ${lastname}ˋ)
 }
+
+// Call the function
+welcomepersontoFES('Rabin', 'Bhandari' );
+welcomepersontoFES('John', 'test' );
