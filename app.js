75fea86647a734d17066e0d720e56964a323de2e
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-03-14 12:11:20 +1100

    prac

--- app.js
+++ app.js
@@ -25,6 +25,15 @@ let user = {
 
 function login(email, password) {
     for (let i =0; i < user.length; ++i ) {
+        if (user[i].email === email) {
+            console.log(user[i]);
+        if (user[i].password === password) {
+            console.log('log the user in - the details are correct')
+        }
+        else {
+            console.log('password is incorrect - try again')
+        }
+        }
         console.log(users[i]);
     } 
 }
