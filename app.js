b44ce8ade7da324e4b1ebdc813a89941686c431f
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-03-14 11:30:03 +1100

    practice question

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+/*
+
+
+*@example
+*['A+', 'A', 'Fail'] => ['A+', 'A']
+*['Fail', 'Fail', 'B'] => ['B']
+*['Fail'] => []
+*/
+
+
 let grades = ['A+', 'A', 'Fail']
 
+let goodGrades = []
 
-let goodGrades = grades.filter(element =>  element != 'Fail')
 
-console.log(goodGrades)
+for (let i = 0; i < grades.length; ++i) {
+ if (grades[i] !== 'Fail') {
+    goodGrades.push(grades[i]);
+ }
+}
 
+console.log(goodGrades);
