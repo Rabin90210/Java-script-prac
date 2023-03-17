eedcf38d8bac70f803a95c5c2e01b510d80e871a
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-03-17 20:34:35 +1100

    prc

--- app.js
+++ app.js
@@ -6,20 +6,7 @@ let usersubscriptionStatus = 'VIP'
 
 let user = {
 {
-    username: 'Rabin',
-    email: 'rabin@blahblah.com',
-    password: 'test123',
-    subscriptionStatus: 'VIP',
-    discordId: 'Disc',
-    lessonscompleted: [0],
-},
-{
-    username: 'sabin',
-    email: 'sabin@blahblah.com',
-    password: 'sad1234',
-    subscriptionStatus: 'VIP',
-    discordId: 'Dsasisc',
-    lessonscompleted: [0, 1, 2, 3],
+  
 },
 };
 
