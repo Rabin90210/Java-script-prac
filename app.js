2353c39879f201c2890f2f8aa75b7dfbc9da0e77
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-04-30 15:11:27 +1000

    JS

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-let userfirstname = 'Rabin'
-let userlastname ='Bhand'
-let userdiscordId = 'disc'
-let usersubscriptionStatus = 'VIP'
+// fetch("https://jasonplaceholder.typicode.com/users/1")
+const emailRef = document.querySelector(".email");
+
+//1. Then
+// fetch("https://jasonplaceholder.typicode.com/users/1")
+//      .then((response)  => {
+//       return response.jason();
+//       })
+//      .then((data)  => {
+//      console.log(data);
+//      rmailRef.innerHTML = data.email
+//      });
 
 
-let user = {
-{
-    username: 'Rabin', blah b
-    email: 'rabin@blahblah.com',
-    password: 'test123',
-    subscriptionStatus: 'VIP',
-    discordId: 'Disc',
-    lessonscompleted: [0],
-},
-{
-    username: 'sabin',
-    email: 'sabin@blahblah.com',
-    password: 'sad1234',
-    subscriptionStatus: 'VIP',
-    discordId: 'Dsasisc',
-    lessonscompleted: [0, 1, 2, 3],
-},
-};
-
-function login(email, password) {
-    for (let i =0; i < user.length; ++i ) {
-        if (user[i].email === email) {
-            console.log(user[i]);
-        if (user[i].password === password) {
-            console.log('log the user in - the details are correct')
-        }
-        else {
-            console.log('password is incorrect - try again')
-        }
-        return;
-     }
-
-    } 
+// 2. Async/Await
+async function main() {
+    const response = await fetch("https://jasonplaceholder.typicode.com/users/1")
+    const data = await response.json
+    console.log (data)
 }
-console.log(users[0]);
 
-login('rabin@blahblah.com', 'test123')
+main():
